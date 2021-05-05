0fa08fcbc5d92586cda9a84a9cd22562eb0ef037
Zio <zi.ao.tan@outlook.com>
2021-05-05 19:53:11 +0000

    some move command added

--- play.js
+++ play.js
@@ -3,4 +3,6 @@ const conn = connect();
 conn.on("connect", () => {
   console.log("Successfully connected to game server");
   conn.write("Name: Zio");
+  // conn.write("Move: up");
+  // setInterval(() => conn.write("Move: left"), 50);
 });
