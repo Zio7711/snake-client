e6f81f2123ba8f77a36fcad8f9865ea5e915cc55
Zio <zi.ao.tan@outlook.com>
2021-05-05 19:44:13 +0000

    name added

--- play.js
+++ play.js
@@ -1,2 +1,6 @@
-const {myNumber, myString, connect} = require("./client")
-connect()
+const {myNumber, myString, connect} = require("./client");
+const conn = connect();
+conn.on("connect", () => {
+  console.log("Successfully connected to game server");
+  conn.write("Name: Zio");
+});
